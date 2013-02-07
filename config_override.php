15c51a512b335d96fb2f40688bf4029202b6a24f
Pralhad Rajgor <pralhad@vitruviantech.com>
2013-02-07 14:33:54 +0530

    changed gitignore to stop ignoring config.php
    Signed-off-by: Pralhad Rajgor <pralhad@vitruviantech.com>

---
+++ config_override.php
@@ -0,0 +1,19 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['email_xss'] = 'YToxMzp7czo2OiJhcHBsZXQiO3M6NjoiYXBwbGV0IjtzOjQ6ImJhc2UiO3M6NDoiYmFzZSI7czo1OiJlbWJlZCI7czo1OiJlbWJlZCI7czo0OiJmb3JtIjtzOjQ6ImZvcm0iO3M6NToiZnJhbWUiO3M6NToiZnJhbWUiO3M6ODoiZnJhbWVzZXQiO3M6ODoiZnJhbWVzZXQiO3M6NjoiaWZyYW1lIjtzOjY6ImlmcmFtZSI7czo2OiJpbXBvcnQiO3M6ODoiXD9pbXBvcnQiO3M6NToibGF5ZXIiO3M6NToibGF5ZXIiO3M6NDoibGluayI7czo0OiJsaW5rIjtzOjY6Im9iamVjdCI7czo2OiJvYmplY3QiO3M6MzoieG1wIjtzOjM6InhtcCI7czo2OiJzY3JpcHQiO3M6Njoic2NyaXB0Ijt9';
+$sugar_config['default_theme'] = 'VTPL';
+$sugar_config['disabled_themes'] = '';
+$sugar_config['addAjaxBannedModules']['0'] = 'Home';
+$sugar_config['addAjaxBannedModules']['1'] = 'Tasks';
+$sugar_config['addAjaxBannedModules']['2'] = 'Meetings';
+$sugar_config['addAjaxBannedModules']['3'] = 'Notes';
+$sugar_config['addAjaxBannedModules']['4'] = 'Leads';
+$sugar_config['addAjaxBannedModules']['5'] = 'ProspectLists';
+$sugar_config['addAjaxBannedModules']['6'] = 'Opportunities';
+$sugar_config['addAjaxBannedModules']['7'] = 'Contacts';
+$sugar_config['addAjaxBannedModules']['8'] = 'Bugs';
+$sugar_config['addAjaxBannedModules']['9'] = 'Accounts';
+$sugar_config['addAjaxBannedModules']['10'] = 'Cases';
+$sugar_config['addAjaxBannedModules']['11'] = 'Prospects';
+$sugar_config['addAjaxBannedModules']['12'] = 'Calls';
+/***CONFIGURATOR***/
