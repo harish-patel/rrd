1de8b5b924ca23583fbe24fb2d033e367d946257
Harish Patel <harish.patel@vitruviantech.com>
2013-02-08 22:35:29 +0530

    updated the enum field's searchview.tpl to show titles also
    Signed-off-by: Harish Patel <harish.patel@vitruviantech.com>

--- include/Smarty/plugins/function.html_options.php
+++ include/Smarty/plugins/function.html_options.php
@@ -2,32 +2,32 @@
 
 /*
 
-Modification information for LGPL compliance
+  Modification information for LGPL compliance
 
-r56990 - 2010-06-16 13:05:36 -0700 (Wed, 16 Jun 2010) - kjing - snapshot "Mango" svn branch to a new one for GitHub sync
+  r56990 - 2010-06-16 13:05:36 -0700 (Wed, 16 Jun 2010) - kjing - snapshot "Mango" svn branch to a new one for GitHub sync
 
-r56989 - 2010-06-16 13:01:33 -0700 (Wed, 16 Jun 2010) - kjing - defunt "Mango" svn dev branch before github cutover
+  r56989 - 2010-06-16 13:01:33 -0700 (Wed, 16 Jun 2010) - kjing - defunt "Mango" svn dev branch before github cutover
 
-r55980 - 2010-04-19 13:31:28 -0700 (Mon, 19 Apr 2010) - kjing - create Mango (6.1) based on windex
+  r55980 - 2010-04-19 13:31:28 -0700 (Mon, 19 Apr 2010) - kjing - create Mango (6.1) based on windex
 
-r54701 - 2010-02-22 09:42:10 -0800 (Mon, 22 Feb 2010) - jmertic - Bug 35849 - Add optional option 'sortoptions' to html_options Smarty function to sort the passed options array. Then I updated that call in the Parent field template so we specify to use this newly added option.
+  r54701 - 2010-02-22 09:42:10 -0800 (Mon, 22 Feb 2010) - jmertic - Bug 35849 - Add optional option 'sortoptions' to html_options Smarty function to sort the passed options array. Then I updated that call in the Parent field template so we specify to use this newly added option.
 
-r51719 - 2009-10-22 10:18:00 -0700 (Thu, 22 Oct 2009) - mitani - Converted to Build 3  tags and updated the build system 
+  r51719 - 2009-10-22 10:18:00 -0700 (Thu, 22 Oct 2009) - mitani - Converted to Build 3  tags and updated the build system
 
-r51634 - 2009-10-19 13:32:22 -0700 (Mon, 19 Oct 2009) - mitani - Windex is the branch for Sugar Sales 1.0 development
+  r51634 - 2009-10-19 13:32:22 -0700 (Mon, 19 Oct 2009) - mitani - Windex is the branch for Sugar Sales 1.0 development
 
-r50375 - 2009-08-24 18:07:43 -0700 (Mon, 24 Aug 2009) - dwong - branch kobe2 from tokyo r50372
+  r50375 - 2009-08-24 18:07:43 -0700 (Mon, 24 Aug 2009) - dwong - branch kobe2 from tokyo r50372
 
-r42807 - 2008-12-29 11:16:59 -0800 (Mon, 29 Dec 2008) - dwong - Branch from trunk/sugarcrm r42806 to branches/tokyo/sugarcrm
+  r42807 - 2008-12-29 11:16:59 -0800 (Mon, 29 Dec 2008) - dwong - Branch from trunk/sugarcrm r42806 to branches/tokyo/sugarcrm
 
-r35784 - 2008-05-20 14:31:40 -0700 (Tue, 20 May 2008) - dwheeler - 21475: Converted multiselect DynamicField default value to be a multiselect. 
+  r35784 - 2008-05-20 14:31:40 -0700 (Tue, 20 May 2008) - dwheeler - 21475: Converted multiselect DynamicField default value to be a multiselect.
 
-r10971 - 2006-01-12 14:58:30 -0800 (Thu, 12 Jan 2006) - chris - Bug 4128: updating Smarty templates to 2.6.11, a version supposedly that plays better with PHP 5.1
+  r10971 - 2006-01-12 14:58:30 -0800 (Thu, 12 Jan 2006) - chris - Bug 4128: updating Smarty templates to 2.6.11, a version supposedly that plays better with PHP 5.1
 
-r8230 - 2005-10-03 17:47:19 -0700 (Mon, 03 Oct 2005) - majed - Added Sugar_Smarty to the code tree.
+  r8230 - 2005-10-03 17:47:19 -0700 (Mon, 03 Oct 2005) - majed - Added Sugar_Smarty to the code tree.
 
 
-*/
+ */
 
 
 /**
@@ -35,7 +35,6 @@ r8230 - 2005-10-03 17:47:19 -0700 (Mon, 03 Oct 2005) - majed - Added Sugar_Smart
  * @package Smarty
  * @subpackage plugins
  */
-
 
 /**
  * Smarty {html_options} function plugin
@@ -60,48 +59,53 @@ r8230 - 2005-10-03 17:47:19 -0700 (Mon, 03 Oct 2005) - majed - Added Sugar_Smart
  */
 function smarty_function_html_options($params, &$smarty)
 {
-    require_once $smarty->_get_plugin_filepath('shared','escape_special_chars');
-    
+    require_once $smarty->_get_plugin_filepath('shared', 'escape_special_chars');
+
     $name = null;
     $values = null;
     $options = null;
     $selected = array();
     $output = null;
-    
+
     $extra = '';
-    
-    foreach($params as $_key => $_val) {
-        switch($_key) {
+
+    foreach ($params as $_key => $_val)
+    {
+        switch ($_key)
+        {
             case 'name':
-                $$_key = (string)$_val;
+                $$_key = (string) $_val;
                 break;
-            
+
             case 'options':
-                $$_key = (array)$_val;
+                $$_key = (array) $_val;
                 break;
-                
+
             case 'values':
             case 'output':
-                $$_key = array_values((array)$_val);
+                $$_key = array_values((array) $_val);
                 break;
 
             case 'selected':
-                $$_key = array_map('strval', array_values((array)$_val));
-                break;	
+                $$_key = array_map('strval', array_values((array) $_val));
+                break;
             case 'multiple':
-            	if ($_val)
-            		$extra .= ' multiple="true" size="6"';
-            	break;
-            	
+                if ($_val)
+                    $extra .= ' multiple="true" size="6"';
+                break;
+
             case 'sortoptions':
                 if ($_val)
                     asort($options);
                 break;
-                
+
             default:
-                if(!is_array($_val)) {
-                    $extra .= ' '.$_key.'="'.smarty_function_escape_special_chars($_val).'"';
-                } else {
+                if (!is_array($_val))
+                {
+                    $extra .= ' ' . $_key . '="' . smarty_function_escape_special_chars($_val) . '"';
+                }
+                else
+                {
                     $smarty->trigger_error("html_options: extra attribute '$_key' cannot be an array", E_USER_NOTICE);
                 }
                 break;
@@ -113,44 +117,51 @@ function smarty_function_html_options($params, &$smarty)
 
     $_html_result = '';
 
-    if (isset($options)) {
-        
-        foreach ($options as $_key=>$_val)
-            $_html_result .= smarty_function_html_options_optoutput($_key, $_val, $selected);
+    if (isset($options))
+    {
 
-    } else {
-        
-        foreach ($values as $_i=>$_key) {
+        foreach ($options as $_key => $_val)
+            $_html_result .= smarty_function_html_options_optoutput($_key, $_val, $selected);
+    }
+    else
+    {
+
+        foreach ($values as $_i => $_key)
+        {
             $_val = isset($output[$_i]) ? $output[$_i] : '';
             $_html_result .= smarty_function_html_options_optoutput($_key, $_val, $selected);
         }
-
     }
 
-    if(!empty($name)) {
+    if (!empty($name))
+    {
         $_html_result = '<select name="' . $name . '"' . $extra . '>' . "\n" . $_html_result . '</select>' . "\n";
     }
 
     return $_html_result;
-
 }
 
-function smarty_function_html_options_optoutput($key, $value, $selected) {
-    if(!is_array($value)) {
-        $_html_result = '<option label="' . smarty_function_escape_special_chars($value) . '" value="' .
-            smarty_function_escape_special_chars($key) . '"';
-        if (in_array((string)$key, $selected))
+function smarty_function_html_options_optoutput($key, $value, $selected)
+{
+    if (!is_array($value))
+    {
+        $_html_result = '<option title="' . smarty_function_escape_special_chars($value) . '" label="' . smarty_function_escape_special_chars($value) . '" value="' .
+                smarty_function_escape_special_chars($key) . '"';
+        if (in_array((string) $key, $selected))
             $_html_result .= ' selected="selected"';
         $_html_result .= '>' . smarty_function_escape_special_chars($value) . '</option>' . "\n";
-    } else {
+    } else
+    {
         $_html_result = smarty_function_html_options_optgroup($key, $value, $selected);
     }
     return $_html_result;
 }
 
-function smarty_function_html_options_optgroup($key, $values, $selected) {
+function smarty_function_html_options_optgroup($key, $values, $selected)
+{
     $optgroup_html = '<optgroup label="' . smarty_function_escape_special_chars($key) . '">' . "\n";
-    foreach ($values as $key => $value) {
+    foreach ($values as $key => $value)
+    {
         $optgroup_html .= smarty_function_html_options_optoutput($key, $value, $selected);
     }
     $optgroup_html .= "</optgroup>\n";
@@ -158,5 +169,4 @@ function smarty_function_html_options_optgroup($key, $values, $selected) {
 }
 
 /* vim: set expandtab: */
-
 ?>
